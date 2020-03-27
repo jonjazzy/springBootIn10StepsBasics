0a9925c7c1c2195395ac1df8aa07b976027ace4c
Jon Jazzy <jonjazzy123@gmail.com>
2020-03-27 11:56:53 -0600

    My FIrst Commit"

---
+++ src/main/java/com/jonjazzy/demo/SpringBootIn10StepsBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.jonjazzy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn10StepsBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIn10StepsBasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonjazzy/demo/SpringBootIn10StepsBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonjazzy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIn10StepsBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
