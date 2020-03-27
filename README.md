a0a448d5fa58f2a9d275dcc79cd200ec52cf1334
Jon Jazzy <jonjazzy123@gmail.com>
2020-03-27 12:24:03 -0600

    My First REST Service

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started
+# Getting Started with SpringBoot in 10 Steps
 
 ### Reference Documentation
 For further reference, please consider the following sections:
---
+++ src/main/java/com/jonjazzy/demo/BooksRESTController.java
@@ -0,0 +1,35 @@
+package com.jonjazzy.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BooksRESTController
+{
+    //return books when someone calls "/books"
+    @GetMapping("/book")
+    public List<Book> getABook()
+    {
+        return Arrays.asList(
+                    new Book(
+                    1l,
+                    "Mastering Spring",
+                    "Authored by John Bello"
+                ));
+    }
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks()
+    {
+        List<Book> myBooks = new ArrayList<Book>();
+        myBooks.add(new Book(1, "Silence of the Lambs", "Charles Darwin"));
+        myBooks.add(new Book(2, "Mirrors on the Ceiling", "The Eagles"));
+        myBooks.add(new Book(3, "Dont walk Run!", "Sarah Palin"));
+
+        return myBooks;
+    }
+}
